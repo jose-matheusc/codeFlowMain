367aad13ee557311f78c7dbb94946de33a6771f7
jose-matheusc <josematheus112@gmail.com>
2024-05-03 20:51:50 -0300

    .

---
+++ codeFlow/src/test/java/br/com/codeFlow/CodeFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codeFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
